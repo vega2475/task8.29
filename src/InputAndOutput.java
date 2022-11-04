dee0bbdbf0a4b7941053b02d25a8876f0362f0e5
vega <devid367552@gmail.com>
2022-11-04 16:57:02 +0300

    v0.01 read matrix from file

--- src/InputAndOutput.java
+++ src/InputAndOutput.java
@@ -1,6 +1,38 @@
-import java.io.File;
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class InputArgs {
-    
+
+import static java.lang.System.out;
+
+public class InputAndOutput {
+
+    void readMtx() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+
+        ArrayList<String> lines = new ArrayList<>();
+        while (br.ready()) {
+            lines.add(br.readLine());
+        }
+        int matrixHeight = lines.size();
+
+        Integer[][] matrix = new Integer[matrixHeight][];
+
+        for(int i = 0; i < matrixHeight; ++i) {
+            String[] nums = lines.get(i).split("\s*,\s*");
+            matrix[i] = new Integer[nums.length];
+            for(int j = 0; j < nums.length; ++j) {
+                matrix[i][j] = Integer.valueOf(nums[j]);
+            }
+        }
+
+        for (int i = 0; i < matrixHeight; ++i) {
+            //System.out.println(Arrays.toString(matrix[i]));
+           out.println(Arrays.toString(matrix[i]).replaceAll("^\\[|]$", ""));
+        }
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        InputAndOutput inputObject = new InputAndOutput();
+        inputObject.readMtx();
     }
 }
